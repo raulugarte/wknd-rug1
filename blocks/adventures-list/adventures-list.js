a269ab85ccad00bed6022816dd1079071f1b9fd9
raulugarte <63106469+raulugarte@users.noreply.github.com>
2023-09-27 14:36:55 +0200

    Update adventures-list.js

--- blocks/adventures-list/adventures-list.js
+++ blocks/adventures-list/adventures-list.js
@@ -23,7 +23,7 @@ export default async function decorate($block) {
 
   // RUG
   var hostnameRUG = link.hostname;
-  var pageRUG = url.substring(url.lastIndexOf('/') + 1);
+  var pageRUG = link.substring(link.lastIndexOf('/') + 1);
   
   if (document.referrer.endsWith('https://exc-unifiedcontent.experience.adobe.net/')) {
     // Assume page is loaded within Universal Editor
